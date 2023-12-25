ff6df819f700ede6065aa2f93c0ff0eed274b669
Aleksandr Potkin <aleksandr.potkin@nedra.digital>
2023-12-25 18:40:21 +0300

    Модалка и сайдбар открываются с анимацией

--- src/pages/ModalPage/ModalPage.tsx
+++ src/pages/ModalPage/ModalPage.tsx
@@ -8,20 +8,14 @@ export const ModalPage = () => {
   return (
     <div>
       <h1>Modal</h1>
-      <button className={styles.buttonOpen} onClick={() => setIsOpen(true)}>
-        Открыть
-      </button>
+
+      <button onClick={() => setIsOpen(true)}>Открыть Modal</button>
+
       <Modal isOpen={isOpen} onClickClose={() => setIsOpen(false)} withCloseButton>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, accusamus nulla.
         Alias, optio, non eos voluptate iusto odit animi delectus dolorum veritatis
         mollitia itaque. Molestiae consequuntur rem eligendi, enim laboriosam fugiat
         assumenda est officia esse suscipit nisi hic magnam obcaecati vel! Accusantium
-        quos necessitatibus sequi aliquam pariatur voluptatem possimus ut non natus
-        dolorum incidunt autem nulla aliquid quasi facere labore a placeat atque eos sint
-        fuga asperiores, tempore alias esse. Suscipit magnam temporibus iusto sunt ex
-        blanditiis voluptatum cupiditate dolore libero vel aspernatur esse, dicta cumque
-        neque commodi voluptas cum, sed nostrum iste. Et debitis ipsam vero doloremque
-        natus id?
       </Modal>
     </div>
   );
