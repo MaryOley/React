691d333d5ae4094b040fd595fd48100961ea7a70
MaryOley <oleynikova.m.a.2000@gmail.com>
2024-01-05 18:20:58 +0300

    x

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { useState } from 'react';
-import style from './app.module.scss';
-import { Sidebar } from './components/Sidebar/Sidebar';
 import { Layout } from './components/Layout/Layout';
 import { Menu } from './components/Menu/Menu';
 import { SidebarPages } from './pages/SidebarPage/SidebarPages';
 import { ModalPage } from './pages/ModalPage/ModalPage';
-import { Button } from './components/Button/Button';
-import { Accardion } from './components/Accardion/Accardion';
 import { CrossAndZero } from './components/CrossAndZero/CrossAndZero';
 import { ButtonPage } from './pages/ButtonPage/ButtonPage';
-import { Slider } from './components/Slider/Slider';
 import { SliderPage } from './pages/SliderPage/SliderPage';
 import { AccardionPage } from './pages/AccardionPage/AccardionPage';
 
